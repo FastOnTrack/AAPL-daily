a75f3d0939564e2d6ef89b9fca9d1b8eee0ac071
FastOnTrack <kevinchen618@gmail.com>
2017-01-04 16:24:29 -0800

    Update AAPL.py

--- AAPL.py
+++ AAPL.py
@@ -10,9 +10,6 @@ import numpy
 try:
     # for Python2
     from Tkinter import *   ## notice capitalized T in Tkinter 
-except ImportError:
-    # for Python3
-    from tkinter import *   ## notice lowercase 't' in tkinter here
 
 @app.route('/')
 def test():
